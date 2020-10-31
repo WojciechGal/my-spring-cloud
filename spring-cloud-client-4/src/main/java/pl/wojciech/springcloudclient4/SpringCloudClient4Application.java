f491a95265b327dbaa948d379d46f7348baa0d5b
Wojciech <wojciech.gladalski@gmail.com>
2020-10-31 11:28:49 +0100

    init of fourth spring cloud client

---
+++ spring-cloud-client-4/src/main/java/pl/wojciech/springcloudclient4/SpringCloudClient4Application.java
@@ -0,0 +1,13 @@
+package pl.wojciech.springcloudclient4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClient4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClient4Application.class, args);
+	}
+
+}
---
+++ spring-cloud-client-4/src/test/java/pl/wojciech/springcloudclient4/SpringCloudClient4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciech.springcloudclient4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudClient4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
